975e2ee3a497ee18d2b58e6e0df9570897c1af4b
pcbt <batutr2@gmail.com>
2018-05-16 19:01:35 +0300

    Reading PIR value added

--- PIR/main.c
+++ PIR/main.c
@@ -8,6 +8,16 @@
 #define F_CPU 16000000UL
 #endif
 
+#define DLREG DDRB
+#define SINREG DDRD
+#define DLPORT PORTB
+#define SINPORT PORTD
+#define DLIN PINB
+#define SININ PIND
+#define DLPIN 5
+#define SINPIN 2
+
+
 /*
 SERIN to D2 pin,
 D/L to D3 pin for reading mode, B5 for interrupt mode.
@@ -22,83 +32,130 @@ void writeregval(unsigned long regval){
     int i;
     unsigned char nextbit;
     unsigned long regmask = 0x1000000;
-
-    DDRD = (1<<3);
-    PORTD = (0<<3);
+    SINREG = (1<<SINPIN);
+    SINPORT = (0<<SINPIN);
     for(i=0;i < 25;i++){
         nextbit = (regval&regmask)!=0;
         regmask >>= 1;
-        //_delay_ms(100);
-        PORTD = (0<<3);
-        //_delay_ms(100);
-        PORTD = (1<<3);
-        //_delay_ms(100);
-        PORTD = (nextbit<<3);
+
+        SINPORT = (0<<SINPIN);
+
+        SINPORT = (1<<SINPIN);
+
+        SINPORT = (nextbit<<SINPIN);
 
         _delay_us(100);
     }
-    PORTD = (0<<3);
+    SINPORT = (0<<SINPIN);
     _delay_us(600);
     puts("writed!");
 }
 
-
-void readlowpowerpyro(void) {
+void readPIR(void){
     int i;
+    int a = 0;
     unsigned int uibitmask;
     unsigned long ulbitmask;
-    PORTD = (1<<2); // Set DL = High, to force fast uC controlled DL read out
-    DDRD = (1<<2); // Configure PORT DL as Output
-    _delay_us(150);
+
+    DLPORT = (1<<DLPIN); // Set DL = High, to force fast uC controlled DL read out
+    DLREG = (1<<DLPIN); // Configure PORT DL as Output
+    _delay_us(110);
     // get first 15bit out-off-range and ADC value
     uibitmask = 0x4000; // Set BitPos
     PIRval = 0;
     for (i=0; i < 15; i++){
         // create low to high transition
-        PORTD = (0<<2); // Set DL = Low, duration must be > 200 ns (tL)
-        DDRD = (1<<2); // Configure DL as Output
+        DLPORT = (0<<DLPIN); // Set DL = Low, duration must be > 200 ns (tL)
+        DLREG = (1<<DLPIN); // Configure DL as Output
         //_delay_us(1);
+        a=a;
         //asm("nop"); // number of nop dependant processor speed (200ns min.)
-        PORTD = (1<<2); // Set DL = High, duration must be > 200 ns (tH)
+        DLPORT = (1<<DLPIN); // Set DL = High, duration must be > 200 ns (tH)
         //_delay_us(1);
-        DDRD = (0<<2); // Configure DL as Input
-        _delay_us(3); // Wait for stable low signal
+        a=a;
+        DLREG = (0<<DLPIN); // Configure DL as Input
+        _delay_us(7); // Wait for stable low signal
         // If DL High set masked bit in PIRVal
-        if (PIND & 0x02) PIRval |= uibitmask;
+
+        if (PINB & (1<<DLPIN)) PIRval |= uibitmask;
+            uibitmask>>=1;
+    }
+    DLPORT = (0<<DLPIN); // Set DL = Low
+    DLREG = (1<<DLPIN); // Configure DL as Output
+    _delay_us(200);
+    DLREG = (0<<DLPIN); // Configure DL as Input
+    PIRval &= 0x3FFF; // clear unused bit
+    printf("%d\n",PIRval);
+    return;
+}
+
+void read(void){
+    int i;
+    int a = 0;
+    unsigned int uibitmask;
+    unsigned long ulbitmask;
+
+    ulbitmask = 0x1000000; // Set BitPos
+    statcfg = 0;
+
+    uibitmask = 0x4000; // Set BitPos
+    PIRval = 0;
+
+    DLPORT = (1<<DLPIN); // Set DL = High, to force fast uC controlled DL read out
+    DLREG = (1<<DLPIN); // Configure PORT DL as Output
+    _delay_us(110);
+    // get first 15bit out-off-range and ADC value
+
+    for (i=0; i < 15; i++){
+        // create low to high transition
+        DLPORT = (0<<DLPIN); // Set DL = Low, duration must be > 200 ns (tL)
+        DLREG = (1<<DLPIN); // Configure DL as Output
+        //_delay_us(1);
+        a=a;
+        //asm("nop"); // number of nop dependant processor speed (200ns min.)
+        DLPORT = (1<<DLPIN); // Set DL = High, duration must be > 200 ns (tH)
+        //_delay_us(1);
+        a=a;
+        DLREG = (0<<DLPIN); // Configure DL as Input
+        _delay_us(4); // Wait for stable low signal
+        // If DL High set masked bit in PIRVal
+
+        if (DLIN & 0x20) PIRval |= uibitmask;
             uibitmask>>=1;
     }
     // get 25bit status and config
-    ulbitmask = 0x1000000; // Set BitPos
-    statcfg = 0;
+
     for (i=0; i < 25; i++){
-        PORTD = (0<<2); // Set DL = Low, duration must be > 200 ns (tL)
-        DDRD = (1<<2); // Configure DL as Output
+        DLPORT = (0<<DLPIN); // Set DL = Low, duration must be > 200 ns (tL)
+        DLREG = (1<<DLPIN); // Configure DL as Output
         //_delay_us(1);
+        a=a;
         //asm("nop"); // number of nop dependant processor speed (200ns min.)
-        PORTD = (1<<2); // Set DL = High, duration must be > 200 ns (tH)
+        DLPORT = (1<<DLPIN); // Set DL = High, duration must be > 200 ns (tH)
         //_delay_us(1);
-        DDRD = (0<<2); // Configure DL as Input
-        _delay_us(3); // Wait for stable low signal, tbd empirically using scope
+        a=a;
+        DLREG = (0<<DLPIN); // Configure DL as Input
+        _delay_us(4); // Wait for stable low signal, tbd empirically using scope
         // If DL High set masked bit
-        if (PIND & 0x02) statcfg |= ulbitmask;
+
+        if (DLIN & 0x20) statcfg |= ulbitmask;
             ulbitmask>>=1;
     }
-    PORTD = (0<<2); // Set DL = Low
-    DDRD = (1<<2); // Configure DL as Output
+    DLPORT = (0<<DLPIN); // Set DL = Low
+    DLREG = (1<<DLPIN); // Configure DL as Output
     //_delay_us(1);
-    DDRD = (0<<2); // Configure DL as Input
+    a=a;
+    DLREG = (0<<DLPIN); // Configure DL as Input
     PIRval &= 0x3FFF; // clear unused bit
+
     if (!(statcfg & 0x60)){
         // ADC source to PIR band pass
         // number in 14bit two's complement
         if(PIRval & 0x2000) PIRval -= 0x4000;
     }
-    //puts("readed!!!!");
-    //puts("readed 3 : %d",3)
-    //printf("hello world %d",3);
+
     printf("PIRVal: %d\tstatcfg: %ul\n",PIRval,statcfg);
     return;
-
 
 }
 int main(void){
@@ -106,7 +163,7 @@ int main(void){
     uart_init();
     stdout = &uart_output;
     stdin  = &uart_input;
-    DDRB = 0x10;
+    //DDRB = 0x10;
     //DDRD |= (0<<2);
     //PORTD |= (1<<2);
     while(1){
@@ -114,17 +171,24 @@ int main(void){
         if(i){
             puts("Writing to register");
             _delay_ms(1000);
-            writeregval(0x00C01710);
+            writeregval(0x00000030);
             i=0;
         }
         //if(!(PIND&0b00000010)){
         //    puts("hareket");
         //}
-        puts("Reading from register");
-        _delay_ms(1000);
-        readlowpowerpyro();
+        //puts("Reading from register");
+        _delay_ms(5);
+        readPIR();
+        //readlowpowerpyro('B',5);
         /*
 
+        if(PINB & 0x20){
+
+            readlowpowerpyro('B',5);
+        }
+        */
+        /*
         if(PINB & 0x20){
 
             puts("Movement!!!\n");
@@ -137,6 +201,7 @@ int main(void){
             DDRB = 0x10;
         }
         */
+
 
     }
     return 0;
